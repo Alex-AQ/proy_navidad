aa8de0fc858a6551d10fd71342ae94c9ec6bc021
Alex-AQ <amarualex83@gmail.com>
2025-12-09 20:50:21 -0400

    Navidad-Proy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Nav.jsx";
 import Inicio from "./Inicio.jsx";
 import Historia from "./Historia.jsx";
@@ -17,41 +17,6 @@ function App() {
         <Route path="/galeria" element={<Galeria />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success shadow">
-      <div className="container-fluid px-5">
-        <Link className="navbar-brand fw-bold fs-4" to="/">
-          🎄 Navidad 2025
-        </Link>
-
-        <button
-          className="navbar-toggler"
-          data-bs-toggle="collapse"
-          data-bs-target="#menu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="menu">
-          <ul className="navbar-nav ms-auto fs-5">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Inicio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/historia">Historia</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tradiciones">Tradiciones</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/galeria">Galería</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contacto">Contacto</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
     </Router>
   );
 }
